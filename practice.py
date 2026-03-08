93442bf3256f8d12f20c615bddd866d98911562f
Mayank Das <mayankdas@Mayanks-MacBook-Air.local>
2026-03-08 17:56:47 -0400

    Simple Math

--- practice.py
+++ practice.py
@@ -4,11 +4,20 @@
 ##Number of contracts
 ##Whether you're long or short (boolean) 
 
-nq = "NQ Futures"
-price = 15134.32
-contracts = 100
-trade = True
-direction = "Long" if trade else "Short"
+# nq = "NQ Futures"
+# price = 15134.32
+# contracts = 100
+# trade = True
+# direction = "Long" if trade else "Short"
 
 
-print(f"Asset = {nq} Price = {price} Contracts = {contracts} Type = {direction}")
+#print(f"Asset = {nq} Price = {price} Contracts = {contracts} Type = {direction}")
+
+price = 15135.52
+entry = 14800
+contracts = 5
+difference = price - entry
+pnl = difference * 5 * 20
+percentage = round(((price - entry) / entry) * 100, 2)x 
+
+print (f"Entry = {entry} | Current Price = {price} | Points Difference = {difference} | P&L = {pnl} | Percentage = {percentage}")
