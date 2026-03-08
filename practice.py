5897dd87f7e14be599f1e54e7404250d6d70a64b
Mayank Das <mayankdas@Mayanks-MacBook-Air.local>
2026-03-08 17:42:55 -0400

    Variables, F Strings

--- practice.py
+++ practice.py
@@ -1,1 +1,14 @@
-print("Hello Markets")
+## Create 4 variables describing a trade you'd make:
+##The asset name
+##Your entry price
+##Number of contracts
+##Whether you're long or short (boolean) 
+
+nq = "NQ Futures"
+price = 15134.32
+contracts = 100
+trade = True
+direction = "Long" if trade else "Short"
+
+
+print(f"Asset = {nq} Price = {price} Contracts = {contracts} Type = {direction}")
